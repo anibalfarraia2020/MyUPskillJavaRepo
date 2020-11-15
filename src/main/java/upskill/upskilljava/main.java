114cadda44bfdef3580d481884a6c4ae4d539d91
Paulo ProenÃ§a <prp@isep.ipp.pt>
2020-11-15 17:11:16 +0000

    Initialize project

---
+++ src/main/java/upskill/upskilljava/main.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upskill.upskilljava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author prp
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        String Grupos[][] = {
+            {  
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome", 
+            }, 
+            {  
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome", 
+            },
+            {  
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome", 
+            },            
+            {  
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome",
+                "Coloque aqui o seu nome", 
+            }
+        };
+        
+        for (int x = 0; x < Grupos.length; x ++) {
+            String Grupo[] = Grupos[x]; 
+            System.out.println( "Grupo " + (x+1));
+            for (int y = 0; y < Grupo.length; y ++) {
+                String formando = Grupo[y];
+                System.out.println( "\t" + formando );
+            }
+            System.out.println("-------");
+        }    
+    }
+}
